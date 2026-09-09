8ff64b00bbcf1bdbcc7c99be36e09c0eb9f0fdf4
Rafael-Flor <2202933@estudante.uab.pt>
2026-09-09 15:29:41 +0100

    refactor: reformulação das definições de escalas e notas

--- src/MusicTheoryModel/music_theory_model.py
+++ src/MusicTheoryModel/music_theory_model.py
@@ -1,36 +1,50 @@
 class Note: #Nota musical
 
-    def __init__(self, pitch):
+    def __init__(self, pitch, name):
+
         if pitch in range(0,128):
             self.pitch=pitch #Tom da nota em semitons
+            self.name=name
+
         else:
             raise ValueError(f"Invalid pitch value {pitch}")
 
 
-    def __add__(self, offset): #Adição de uma nota com um inteiro resulta na elevação do seu tom por esse valor
-        if isinstance(offset, int):
-            return Note(self.pitch+offset)
-        else:
-            return NotImplemented
-
-
 class ScaleType: #Formato genérico para um tipo de escala
-    def __init__(self, name, scale_degrees, note_offsets):
+    def __init__(self, name, scale_degrees, note_steps):
         self.name=name
         self.scale_degrees = scale_degrees #graus da escala
-        self.note_offsets=note_offsets #distancia de cada nota da escala à tónica
+        self.note_steps=note_steps #intervalos entre graus da escala em semitons
 
 
 class MusicTheoryModel:
-    TONICS = [Note(0), Note(1), Note(2), Note(3), Note(4), Note(5), Note(6), Note(7), Note(8), Note(9), Note(10), Note(11)] #Tónicas possiveis
+    NATURAL_NOTE_NAMES=["C","D","E","F","G","A","B"]
+    TONICS = [
+        Note(0, "C"),
+        Note(1, "C#"),
+        Note(1, "Db"),
+        Note(2,"D"),
+        Note(3, "D#"),
+        Note(3, "Eb"),
+        Note(4, "E"),
+        Note(5, "F"),
+        Note(6, "F#"),
+        Note(6, "Gb"),
+        Note(7, "G"),
+        Note(8, "G#"),
+        Note(8, "Ab"),
+        Note(9, "A"),
+        Note(10, "A#"),
+        Note(10, "Bb"),
+        Note(11, "B")] #Tónicas possiveis
     def __init__(self):
         self.scale_types=[ #Definição dos tipos de escala do modelo
-            ScaleType("Major", [0, 1, 2, 3, 4, 5, 6], [0, 2, 4, 5, 7, 9, 11, 12]),
-            ScaleType("Minor", [0, 1, 2, 3, 4, 5, 6], [0, 2, 3, 5, 7, 8, 10, 12]),
-            ScaleType("Major Pentatonic", [0, 1, 2, 4, 5], [0, 2, 4, 7, 9]),
-            ScaleType("Minor Pentatonic", [0, 2, 3, 4, 6], [0, 3, 5, 7, 10]),
-            ScaleType("Mixolydian", [0, 1, 2, 3, 4, 5, 6], [0, 2, 4, 5, 7, 9, 10, 12]),
-            ScaleType("Dorian", [0, 1, 2, 3, 4, 5, 6],[0, 2, 3, 5, 7, 9, 10, 12])
+            ScaleType("Major", [1, 2, 3, 4, 5, 6, 7], [0, 2, 2, 1, 2, 2, 2, 1]),
+            ScaleType("Minor", [1, 2, 3, 4, 5, 6, 7], [0, 2, 1, 2, 2, 1, 2, 2]),
+            ScaleType("Major Pentatonic", [1, 2, 3, 5, 6], [0, 2, 2, 3, 2]),
+            ScaleType("Minor Pentatonic", [1, 3, 4, 5, 7], [0, 3, 2, 2, 3]),
+            ScaleType("Mixolydian", [1, 2, 3, 4, 5, 6, 7], [0, 2, 2, 1, 2, 2, 1, 2]),
+            ScaleType("Dorian", [1, 2, 3, 4, 5, 6, 7],[0, 2, 1, 2, 2, 2, 1, 2])
         ]
 
 
